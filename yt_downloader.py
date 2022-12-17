5de04ced7e2113b2cd0ad36afdd08a651eb878d2
ThinhLe881 <thinhleminh201@gmail.com>
2022-12-17 12:09:45 -0500

    fix bugs, rewrite some functions

--- yt_downloader.py
+++ yt_downloader.py
@@ -1,8 +1,7 @@
+import argparse
 from pytube import YouTube
 import ffmpeg
-from sys import argv
-from pathlib import Path
-
+import os
 
 def clean_filename(name):
     forbidden_chars = '"*\\/\'.|?:<>'
@@ -11,40 +10,78 @@ def clean_filename(name):
         filename = filename[:170] + '...'
     return filename
 
-dest_path = argv[1]
-link = argv[2]
-if len(argv) > 3:
-    res = argv[3]
+def download_video(yt, res):
+    print('Title: ' + yt.title)
+    print('Downloading...')
 
-yt = YouTube(link, use_oauth=True, allow_oauth_cache=True)
+    current_path = os.getcwd()
+    vid_path = rf'{current_path}\video.mp4'
+    aud_path = rf'{current_path}\audio.mp3'
+    vid_res = ['2160p', '1440p', '1080p', '720p', '480p', '360p', '240p']
+    aud_res = ['256kbps', '128kbps', '48kbps']
 
-print('Title: ' + yt.title)
-print('Save as: ' + dest_path)
-print('Downloading...')
+    # If no resolution is provided, choose the highest resolution available
+    res_flag = True
+    if res == '':
+        res_flag = False
 
-vid_path = Path(__file__).parent / './video.mp4'
-aud_path = Path(__file__).parent / './audio.mp3'
-dynamic_streams = ['2160p|160kbps', '1440p|160kbps', '1080p|160kbps', '720p|160kbps', '720p|128kbps', '480p|160kbps', '480p|128kbps', '360p|160kbps', '360p|128kbps', '240p|160kbps', '240p|128kbps']
+    vid_flag = False
+    for vi_res in vid_res:
+        try:
+            if res_flag:
+                if vi_res != res:
+                    continue
+            yt.streams.filter(res=vi_res, only_video=True, progressive=False).first().download(filename=vid_path)
+            vid_flag = True
+            break
+        except (Exception):
+            continue
 
-flag = False
-for ds in dynamic_streams:
+    if not vid_flag:
+        print('No suitable video stream found')
+        exit(1)
+
+    aud_flag = False
+    for au_res in aud_res:
+        try:
+            yt.streams.filter(abr=au_res, only_audio=True, progressive=False).first().download(filename=aud_path)
+            aud_flag = True
+        except (Exception):
+            continue
+    
+    if not aud_flag:
+        print('No suitable audio stream found')
+        exit(1)
+
+    print('Download complete')
+    return vid_path, aud_path
+
+def save_video(yt, dest_path, vid_path, aud_path):
+    print('Save as: ' + dest_path)
     try:
-        if len(argv) > 3:
-            if ds.split('|')[0] != res:
-                continue
-        yt.streams.filter(res=ds.split('|')[0], progressive=False).first().download(filename=vid_path)
-        yt.streams.filter(abr=ds.split('|')[1], progressive=False).first().download(filename=aud_path)
-        flag = True
-        break
-    except (Exception):
-        continue
+        video = ffmpeg.input(vid_path)
+        audio = ffmpeg.input(aud_path)
+        filename = rf'{dest_path}\{clean_filename(yt.title)}.mp4'
+        stream = ffmpeg.output(audio, video, filename)
+        ffmpeg.run(stream)
+        print('Done')
+    except:
+        print('Can not save the video')
 
-if not flag:
-    print('No suitable stream found')
-    exit(1)
+def main():
+    parser = argparse.ArgumentParser(description='YouTube Video Downloader')
+    parser.add_argument('dest_path', metavar='dest_path', type=str, help='The destination folder')
+    parser.add_argument('link', metavar='link', type=str, help='The link to the YouTube video you want to download')
+    parser.add_argument('--res', metavar='res', type=str, required=False, default='', help='The resolution of the YouTube video you want to download')
+    args = parser.parse_args()
 
-video = ffmpeg.input(vid_path)
-audio = ffmpeg.input(aud_path)
-filename = dest_path + './' + clean_filename(yt.title) + '.mp4'
-ffmpeg.output(audio, video, filename).run()
-print('Download complete')
+    dest_path = args.dest_path
+    link = args.link
+    res = args.res
+
+    yt = YouTube(link, use_oauth=True, allow_oauth_cache=True)
+    vid_path, aud_path = download_video(yt, res)
+    save_video(yt, dest_path, vid_path, aud_path)   
+
+if __name__ == '__main__':
+    main()
